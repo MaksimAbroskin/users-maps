09604ca7320fe17a8023cd9c0d8678b86553e841
maks425mirniy@gmail.com <3ns3aWmPUPT2DwJ>
2021-05-20 23:21:24 +0300

    Small refactoring. Fixed tests

--- src/main/scala/ru/dins/scalaschool/users_maps/Config.scala
+++ src/main/scala/ru/dins/scalaschool/users_maps/Config.scala
@@ -6,9 +6,9 @@ object Config {
     lazy val postgres = "postgres"
 
     lazy val host: String = sys.env.getOrElse("DB_HOST", postgres)
-    lazy val port: String = sys.env.getOrElse("DB_HOST", "5432")
+    lazy val port: String = sys.env.getOrElse("DB_PORT", "5432")
     lazy val name: String = sys.env.getOrElse("DB_NAME", postgres)
-    lazy val user: String = sys.env.getOrElse("DB_NAME", postgres)
-    lazy val pass: String = sys.env.getOrElse("DB_NAME", postgres)
+    lazy val user: String = sys.env.getOrElse("DB_USER", postgres)
+    lazy val pass: String = sys.env.getOrElse("DB_PASS", postgres)
   }
 }
